f9c56803de75f7b1c7c0649ae7adc0cfb0566b64
Dean Mulrooney <mulrooney.dean@gmail.com>
2020-04-03 18:38:47 +0100

    Route files added

--- node/servers/knexfile.js
+++ node/servers/knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
     client: 'postgresql',
     connection: {
       host: '127.0.0.1',
-      user: '',
-      password: '',
+      user: 'deanmulrooney',
+      password: 'm8ijk9mkkm',
       port: '5432',
       database: 'grpc_products',
     },
